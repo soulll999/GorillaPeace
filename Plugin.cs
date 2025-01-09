c95324eefd28e7bb2354681c2c6ed26e72f7a44a
Soul <122072344+soulll999@users.noreply.github.com>
2025-01-09 18:08:06 +1100

    Add files via upload

---
+++ Plugin.cs
@@ -0,0 +1,20 @@
+﻿using BepInEx;
+namespace GorillaPeace
+{
+    [BepInPlugin(PluginInfo.GUID, PluginInfo.Name, PluginInfo.Version)]
+    public class Plugin : BaseUnityPlugin
+    {
+        public void Update()
+        {
+            foreach (GorillaPlayerScoreboardLine line in GorillaScoreboardTotalUpdater.allScoreboardLines)
+            {
+                if (!line.muteButton.isOn)
+                {
+                    line.PressButton(true, GorillaPlayerLineButton.ButtonType.Mute);
+                    
+                    line.muteButton.UpdateColor();
+                }
+            }
+        }
+    }
+}
---
+++ PluginInfo.cs
@@ -0,0 +1,12 @@
+﻿namespace GorillaPeace
+{
+    /// <summary>
+    /// This class is used to provide information about your mod to BepInEx.
+    /// </summary>
+    class PluginInfo
+    {
+        public const string GUID = "com.soul.gorillatag.gorillapeace";
+        public const string Name = "GorillaPeace";
+        public const string Version = "1.0.0";
+    }
+}
